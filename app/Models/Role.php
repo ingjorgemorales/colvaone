8e9bfaa37b9a245c224d89971e7750555a144778
Jorge Andrés Morales <mjorge801@yahoo.com>
2026-07-31 22:31:36 -0500

    Fix Role model: users relationship uses role string column, not role_id

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -18,6 +18,6 @@ class Role extends Model
 
     public function users(): HasMany
     {
-        return $this->hasMany(User::class);
+        return $this->hasMany(User::class, 'role', 'slug');
     }
 }
