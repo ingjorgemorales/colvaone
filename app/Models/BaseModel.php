28296686c3fddd4596f29389a6fa45745927267a
Jorge Andrés Morales <mjorge801@yahoo.com>
2026-08-04 20:07:28 -0500

    Fix Bogota timezone handling

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,17 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
 
 class BaseModel extends Model
 {
-    public function getCreatedAtAttribute($value)
-    {
-        return $value ? Carbon::parse($value, 'UTC')->setTimezone('America/Bogota') : null;
-    }
-
-    public function getUpdatedAtAttribute($value)
-    {
-        return $value ? Carbon::parse($value, 'UTC')->setTimezone('America/Bogota') : null;
-    }
 }
